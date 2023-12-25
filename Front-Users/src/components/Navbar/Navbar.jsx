77af5810772c66bc353388763f174efb5b0fced7
Juan <juangui9508@hotmail.com>
2023-12-24 20:08:25 -0500

    change navbar

--- Front-Users/src/components/Navbar/Navbar.jsx
+++ Front-Users/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-// import {Link} from "react-router-dom";
-import style from "../Navbar/NavBar.module.css"
+import {Link} from "react-router-dom";
+// import style from "../Navbar/NavBar.module.css"
 
 const NavBar = () =>{
   return(
 
    <div className={style.mainContainer}>
-    {/* <Link to="/home">HOME</Link>
+    <Link to="/home">HOME</Link>
     <Link to="/create">FORM</Link>
- */}
+
 
    </div>
   )
