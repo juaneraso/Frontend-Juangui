0b130e7229ee178edc068c1d0d2ede4672a7481b
Juan <juangui9508@hotmail.com>
2023-12-26 19:17:40 -0500

    console.log

--- Front-Users/src/redux/actions.js
+++ Front-Users/src/redux/actions.js
@@ -6,6 +6,7 @@ export const GET_USER = "GET_USER";
 // Variable de entorno
 
 const back = import.meta.env.VITE_APP_BACK;
+console.log("Hola estoy aqui");
 
 // export const getUsers = () => {
 //   return async function (dispatch) {
