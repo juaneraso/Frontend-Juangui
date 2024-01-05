55116d3251feff73b49f7fdac47156fa49ece997
Juan <juangui9508@hotmail.com>
2024-01-04 22:35:32 -0500

    title

--- Front-Users/src/components/CardsContainer/CardsContainer.jsx
+++ Front-Users/src/components/CardsContainer/CardsContainer.jsx
@@ -6,8 +6,6 @@ import { useSelector } from "react-redux";
 const CardsContainer = () =>{
     const users  = useSelector(state=>state.users);
    
-    
-
 
     return (
      <div className={style.container}>
